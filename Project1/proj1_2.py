ab2356489210789176a0b25228b13debaf69296f
Gadangadang <sakarias.frette@outlook.com>
2021-03-12 18:48:04 +0100

    Changed task 2, added extra test

--- Project1/proj1_2.py
+++ Project1/proj1_2.py
@@ -1,20 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def U(x):
-    return -(omega_m/x + omega_w*x**2)
+def U(x, om, ow):
+    return -(om/x + ow*x**2)
 
-def E(x):
-    return 1 - omega_m - omega_w + x - x
+def E(x, om, ow):
+    return 1 - om - ow + x - x
 
 x = np.linspace(0.001,1,100)
 omega_m = 0.24
 omega_w = 0.64
+omega_m2 = 1.1
+omega_w2 = 1.5
 
-plt.plot(x,U(x), label = 'U')
-plt.plot(x, E(x), label = 'E')
-plt.title("omega_m0: {}, omega_w0: {}".format(omega_m, omega_w))
-plt.xlabel(r"$\Omega_{m0}$")
-plt.ylabel(r"$\Omega_{w0}$")
+ediff = E(x , omega_m, omega_w) - U(x, omega_m, omega_w)
+ediff2 = E(x , omega_m2, omega_w2) - U(x, omega_m2, omega_w2)
+
+if ediff.any() < 0:
+    print("This combo is bad for E")
+else:
+    print("Is all good for E")
+
+if  ediff2.any() < 0:
+    print("This combo is bad for E2")
+else:
+    print("Is all good for E2")
+
+
+plt.plot(x,U(x, omega_m, omega_w), label = 'U')
+plt.plot(x,U(x, omega_m2, omega_w2), label = 'U2')
+plt.plot(x, E(x , omega_m, omega_w), label = 'E')
+plt.plot(x, E(x, omega_m2, omega_w2), label = 'E2')
+
+plt.xlabel("x")
+plt.ylabel("E and U [energy]")
 plt.legend()
 plt.show()
