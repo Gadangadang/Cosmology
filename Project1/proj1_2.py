15b7a9e73bef3a1dcd93c8e74f911b0c23e840b9
Gadangadang <sakarias.frette@outlook.com>
2021-03-13 19:14:43 +0100

    Changed parameters
    for different values of omega w, m, and w

--- Project1/proj1_2.py
+++ Project1/proj1_2.py
@@ -2,29 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def U(x, om, ow):
-    return -(om/x + ow*x**2)
+    return -(om/x + ow*x**(-(1+3*w)))
 
 def E(x, om, ow):
     return 1 - om - ow + x - x
 
 x = np.linspace(0.001,1,100)
-omega_m = 0.24
-omega_w = 0.64
-omega_m2 = 1.1
-omega_w2 = 1.5
+omega_m = 0
+omega_w = 3
+omega_m2 = 2
+omega_w2 = 1
+w = -1
 
-ediff = E(x , omega_m, omega_w) - U(x, omega_m, omega_w)
-ediff2 = E(x , omega_m2, omega_w2) - U(x, omega_m2, omega_w2)
-
-if ediff.any() < 0:
-    print("This combo is bad for E")
-else:
-    print("Is all good for E")
-
-if  ediff2.any() < 0:
-    print("This combo is bad for E2")
-else:
-    print("Is all good for E2")
 
 
 plt.plot(x,U(x, omega_m, omega_w), label = 'U')
